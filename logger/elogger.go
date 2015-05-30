c65368ef103fbf7bb584e9ad451c2125d23fab11
jingleizhang <jingleizhang@creditease.cn>
2015-05-30 20:21:07 +0800

    fix

--- logger/elogger.go
+++ logger/elogger.go
@@ -5,13 +5,13 @@ import (
 )
 
 func init() {
-	statLogger = base.NewSingleton(newStatLogger)
+	statLogger = elog.NewSingleton(newStatLogger)
 }
 
 var statLogger elog.Singleton
 
 func newStatLogger() (interface{}, error) {
-	logger := elog.NewELog("stat_logger", baselog.LOG_SHIFT_BY_DAY, 2*1024*1024, elog.LOG_FATAL|elog.LOG_ERROR|elog.LOG_INFO)
+	logger := elog.NewELog("stat_logger", elog.LOG_SHIFT_BY_DAY, 2*1024*1024, elog.LOG_FATAL|elog.LOG_ERROR|elog.LOG_INFO)
 	logger.SetKeptInFile(true)
 	return logger, nil
 }
